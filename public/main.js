d67328c68d154336ae935adb5882369b9c12085d
Fuzen-py <fuzen@protonmail.com>
2017-08-07 00:02:05 -0400

    Fix: Use a proper random word api

--- public/main.js
+++ public/main.js
@@ -1,98 +1,102 @@
-// TODO: Async API Request
-// TODO: Use a https API
-(function() {
+(function () {
+    const MIN_LENGTH = 4
+    const API_BASE = "https://fuzen.info"
+    const API_URL = `${API_BASE}/randword/${MIN_LENGTH}`
+    const wordLine = document.querySelector('#word_line');
+    const remainingSelector = document.querySelector('#remaining');
+    const highScoreSelector = document.querySelector("#high_score");
+    const validKeys = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let high_score = 0;
+    // Blanks is a string
+    let word;
+    let blanks;
+    let attemptsRemaining;
 
-const apiURL = "http://setgetgo.com/randomword/get.php"
-// Minimum Length
-const minLength = 4;
-const wordLine = document.querySelector('#word_line');
-const remainingSelector = document.querySelector('#remaining');
-const highScoreSelector = document.querySelector("#high_score");
-
-let highScore = 0;
-
-// Gets a random word and returns it as an array
-function getWord() {
-    let request = new XMLHttpRequest();
-    request.open("GET", apiURL, false)
-    request.send(null)
-    return request.responseText.toLocaleLowerCase().split(''); 
-}
-
-// Displays a loss screen
-function lossScreen() {
-    wordLine.innerText = `You lose, it was "${word.join('')}"`;
-    remainingSelector.innerText = '';
-}
-
-// Displays a win screen
-function winScreen() {
-    score = attemptsRemaining / (word.length + 2) * 100 >> 0
-    wordLine.innerText = `"${word.join('')}" You win!`;
-    remainingSelector.innerText = `Score: ${score}`;
-    if (score <= highScore) {
-        return
+    function updateDisplay() {
+        wordLine.innerText = blanks.join(' ');
+        remainingSelector.innerText = attemptsRemaining;
     }
-    highScoreSelector.innerText = score;
-    highScore = score;
-}
-
-let word = getWord();
-// Creates an array of `_` of the same length of word
-let blanks = Array(word.length).fill('_');
-let attemptsRemaining = word.length + 2;
-// Array of valid keys
-let validKeys = 'abcdefghijklmnopqrstuvwxyz'.split('');
-
-// Updates the page to show the new content
-function doRender() {
-    wordLine.innerText = blanks.join(' ');
-    remainingSelector.innerText = attemptsRemaining;
-}
-
-function reset() {
-    word = getWord();
-    blanks = Array(word.length).fill('_');
-    attemptsRemaining = word.length + 2;
-    doRender()
-}
-
-function onKeyPress(e) {
-    if (validKeys.includes(e.key.toLowerCase())) {
-        let letter = e.key;
-        if (attemptsRemaining === 0 || blanks.join('') === word.join('')) {return}
-        // if letter is in word
-        if (word.includes(letter.toLowerCase())) {
-            // Edit blanks
-            for (var i = 0; i < word.length; i++) {
-                if (letter === word[i]) {
-                    blanks[i] = letter;
-                }
-            }
-            // Compare if blanks is the same as word
-            if (blanks.join('') === word.join('')) {
-                return winScreen()
-            }
-            // Render if not
-            return doRender()
-        }
-        // If letter is not in word
-        attemptsRemaining--
-        if (attemptsRemaining === 0) {
-            return lossScreen()
-        }
-        return doRender();
+        // Displays a loss screen
+    function lossScreen() {
+        wordLine.innerText = `You lose, it was "${word}"`;
+        remainingSelector.innerText = '';
     }
+
+    // Displays a win screen
+    function winScreen() {
+        score = attemptsRemaining / (word.length + 2) * 100 >> 0
+        wordLine.innerText = `"${word}" You win!`;
+        remainingSelector.innerText = `Score: ${score}`;
+        if (score <= highScore) {
+            return
+        }
+        highScoreSelector.innerText = score;
+        highScore = score;
+    }
+
     // Reset
-    if ('`' === e.key) {
-        reset()
+    function reset() {
+        document.body.removeEventListener('keypress', onKeyPress)
+        word = undefined;
+        blanks = undefined;
+        attemptsRemaining = undefined;
+        getWord(); // Call getWord again
     }
-}
+    function onKeyPress(e) {
+        if (validKeys.includes(e.key.toLowerCase())) {
+            let letter = e.key;
+            if (attemptsRemaining === 0 || blanks.join('') === word) {
+                return;
+            }
+            // if letter is in word
+            if (word.includes(letter.toLowerCase())) {
+                // Edit blanks
+                for (var i = 0; i < word.length; i++) {
+                    if (letter === word[i]) {
+                        blanks[i] = letter;
+                    }
+                }
+                // Compare if blanks is the same as word
+                if (blanks.join('') === word) {
+                    return winScreen();
+                }
+                // update display if not
+                return updateDisplay();
+            }
+            // If letter is not in word
+            attemptsRemaining--;
+            if (attemptsRemaining === 0) {
+                return lossScreen();
+            }
+            return updateDisplay();
+        }
+        // Reset
+        if ('`' === e.key) {
+            reset();
+        }
+    }
 
-// Update render
-doRender()
-
-// Register Keypress Handler
-document.body.addEventListener('keypress', onKeyPress)
+    function init(resp) {
+        // Store the response
+        word = resp.responseText.toLowerCase();
+        blanks = Array(word.length).fill('_');
+        attemptsRemaining = word.length + 2;
+        document.body.addEventListener('keypress', onKeyPress);
+        updateDisplay();
+    }
+    // Gets a new word then passes it into init
+    function getWord() {
+        let req = new XMLHttpRequest();
+        wordLine.innerText = "Please wait...";
+        remainingSelector.innerText = "";
+        req.open("GET", API_URL, true)
+        req.onreadystatechange = function () {
+            if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
+                init(req)
+            }
+        };
+        req.send();
+    }
+    getWord()
 
 }())
